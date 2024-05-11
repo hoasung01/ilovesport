f9a9f23d2b021b487fc409efe9541a4431bc3753
hoasung01 <nguyenngochai.shipagent@gmail.com>
2024-05-11 14:19:22 +0700

    Polish admin page with bootstrap

--- Gemfile
+++ Gemfile
@@ -67,3 +67,8 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "sassc-rails"
+gem "bootstrap"
+
+
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  layout "admin" # Specify the 'admin' layout
+  before_action :authenticate_admin!
+
   def index
   end
 end
