afcf069787fdf8901a29c1f71b6bbe4f94817ead
hoasung01 <nguyenngochai.shipagent@gmail.com>
2024-08-29 21:22:54 +0700

    Add location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_28_152645) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_29_142234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_28_152645) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_facility_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_facility_owners_on_reset_password_token", unique: true
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.float "latitude"
+    t.float "longtitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sports", force: :cascade do |t|
