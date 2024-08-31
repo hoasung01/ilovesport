36f7bb2808db8bb94af19ee7a583f0e9c88f110d
hoasung01 <nguyenngochai.shipagent@gmail.com>
2024-08-31 12:43:30 +0700

    Polish template

--- app/controllers/admin/facility_owners_controller.rb
+++ app/controllers/admin/facility_owners_controller.rb
@@ -3,6 +3,6 @@ class Admin::FacilityOwnersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @facility_owners = FacilityOwner.all
+    @facility_owners = FacilityOwner.includes(training_grounds: :sport)
   end
 end
