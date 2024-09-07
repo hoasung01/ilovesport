1b0aeba695cc1ba2a92d6b06b1b5de97efb59ac9
hoasung01 <nguyenngochai.shipagent@gmail.com>
2024-09-07 22:04:10 +0700

    Add search function

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -1,5 +1,16 @@
 class VisitorController < ApplicationController
   def index
-    @training_grounds = TrainingGround.includes(:sport, :location).page(params[:page]).per(10)
+    @training_grounds = TrainingGround.includes(:sport, :location)
+
+    if params[:search].present?
+      @training_grounds = @training_grounds.where("training_grounds.name ILIKE ? OR locations.address ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+        .joins(:location)
+    end
+
+    if params[:sport_type].present?
+      @training_grounds = @training_grounds.where(sport_id: params[:sport_type])
+    end
+
+    @training_grounds = @training_grounds.page(params[:page]).per(10)
   end
 end
