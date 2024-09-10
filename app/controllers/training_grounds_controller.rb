f5474033c146755b6487d2bd2953acb53d94d79f
hoasung01 <nguyenngochai.shipagent@gmail.com>
2024-09-11 05:57:00 +0700

    Add booking ground in user page

--- app/controllers/training_grounds_controller.rb
+++ app/controllers/training_grounds_controller.rb
@@ -1,5 +1,6 @@
 class TrainingGroundsController < ApplicationController
   def show
     @training_ground = TrainingGround.find(params[:id])
+    @booking = Booking.new # Initialize a new booking object
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :facility_owners
 
-  resources :training_grounds, only: [:show]
+  resources :training_grounds, only: [:show] do
+    resources :bookings, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
